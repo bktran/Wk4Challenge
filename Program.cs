2c650f80cbae15279261ec69c852bdba2ff9a53f
Bao Khanh Tran <ktbv.22@outlook.com>
2026-02-08 00:16:38 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Wk4Challenge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 7201432;
+        }
+
+        public static bool IfNumberContains3(int num) 
+        {
+
+
+            return false;
+        }
+    }
+}
